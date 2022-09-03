1d902509ba078fc1e6b6899c4aabad6121fd365d
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-03 12:16:13 +0200

    apollo setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 // imports
-import { Routes, Route } from "react-router-dom"
-import "./App.scss"
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // component imports
-import Header from './components/Header';
+import Header from "./components/Header";
 
 // page imports
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Results from "./pages/Results"
-import Hotel from "./pages/Hotel"
-import Login from "./pages/Login"
-import Admin from "./pages/Admin"
-import Owner from "./pages/Owner"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Results from "./pages/Results";
+import Hotel from "./pages/Hotel";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import Owner from "./pages/Owner";
+
+// apollo client
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      
-      <Routes>
+    <>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
 
+          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
@@ -32,9 +41,10 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/owner" element={<Owner />} />
-
-      </Routes>
-    </div>
+          </Routes>
+        </div>
+      </ApolloProvider>
+    </>
   );
 }
 

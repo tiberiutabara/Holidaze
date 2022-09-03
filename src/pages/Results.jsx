8e92caec8c25cecbe4b1a9ed47759898efe30d74
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-03 11:16:10 +0200

    First Step

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
 
 export default function Results() {
+
+  const { loading, error, data } = useFetch('http://localhost:1337/api/hotels?populate=*')
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error.</p>
+
   return (
-    <div>Results</div>
+    <div className='results'>
+      {data.map(hotel => (
+        <div key={hotel.id} className="hotel-card">
+          <h3>{hotel.attributes.Title}</h3>
+          <p>{hotel.attributes.Price}</p>
+
+          <Link to={`/Hotel/${hotel.id}`}>Details</Link>
+          <br /> <br />
+        </div>
+      ))}
+    </div>
   )
 }

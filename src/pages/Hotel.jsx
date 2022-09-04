fde5eec2ecddf44b505398403bacba0d3a2aa025
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-04 18:10:21 +0200

    first step

--- src/pages/Hotel.jsx
+++ src/pages/Hotel.jsx
@@ -5,6 +5,8 @@ export default function Hotel() {
   const { id } = useParams()
   const { loading, error, data } = useFetch('http://localhost:1337/api/hotels/' + id + '?populate=*')
 
+  const prefix = 'http://localhost:1337';
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error.</p>
 
@@ -16,6 +18,16 @@ export default function Hotel() {
           <p>{data.attributes.Price}</p>
 
           <p>{data.attributes.Description}</p>
+
+          <p>Thumbnail:</p>
+
+          <img src={prefix + data.attributes.Thumbnail.data.attributes.url} alt={data.attributes.Title} />
+
+          <p>Gallery:</p>
+
+          <p>{data.attributes.Gallery.data.map(img => (
+            <img key={img.id} src={prefix + img.attributes.url} alt={img.attributes.name}/>
+          ))}</p>
     </div>
   )
 }

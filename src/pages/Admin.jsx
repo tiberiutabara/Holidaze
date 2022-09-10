c2b4ba9e2ad971650db73f8fe58f500fe1f9dffc
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-10 16:39:19 +0200

    Contact form functionality

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,8 +1,30 @@
  import React from 'react'
+ import useFetch from '../hooks/useFetch'
  
  export default function Admin() {
+
+  const { loading, error, data } = useFetch('http://localhost:1337/api/messages')
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error.</p>
+
    return (
-     <div>Admin</div>
+     <div>
+
+      <h1>Admin</h1>
+
+      {data.map(message => (
+
+        <div key={message.id} className="message-card">
+        <h3>{message.attributes.subject}</h3>
+        <p>by {message.attributes.name}</p>
+
+        <br /> <br />
+        </div> 
+
+      ))}
+
+     </div>
    )
  }
  
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,115 @@
-import React from 'react'
+import { useState } from "react";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [category, setCategory] = useState("Hotel Listings");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    async function addMessage() {
+      const messageContent = {
+        name: name,
+        email: email,
+        subject: subject,
+        category: category,
+        message: message,
+      };
+
+      const add = await fetch("http://localhost:1337/api/messages", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: messageContent }),
+      });
+
+      const addResponse = await add.json();
+
+      console.log(addResponse);
+    }
+
+    addMessage();
+
+    setName("");
+    setEmail("");
+    setSubject("");
+    setCategory("Hotel Listings");
+    setMessage("");
+  };
+
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+
+      <h1>For collaboration proposals,</h1>
+      <h2>Don't heistate to get in touch</h2>
+      <br /> <br />
+
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <label>
+          {" "}
+          <span> Your Name / Company</span>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+
+        <br /> <br />
+        <label>
+          {" "}
+          <span> Your Email</span>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+
+        <br /> <br />
+        <label>
+          {" "}
+          <span> Subject Title</span>
+          <input
+            type="text"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            required
+          />
+        </label>
+
+        <br /> <br />
+        <label>
+          {" "}
+          <span> Choose Category</span>
+          <select onChange={(e) => setCategory(e.target.value)} required>
+            <option value="Hotel Listings">Hotel Listings</option>
+            <option value="Bug repport">Bug repport</option>
+            <option value="Career">Career</option>
+          </select>
+        </label>
+
+        <br /> <br />
+        <label>
+          {" "}
+          <span> Message</span>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </label>
+        
+        <br /> <br />
+        <button>Send message</button>
+      </form>
+    </div>
+  );
 }

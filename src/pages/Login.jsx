e296481e109bc81a54b66b86c5ca8f1501b1bd66
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-13 14:19:18 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,49 +1,72 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const userAuthentification = async () => {
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
     const loginData = {
-      identifier: "admin@holidaze.com",
-      password: "test1234",
+      identifier: username,
+      password: password,
     };
-  
+
     const login = await fetch(`http://localhost:1337/api/auth/local`, {
       method: "POST",
-  
+
       headers: {
         Accept: "application/json",
-  
+
         "Content-Type": "application/json",
       },
-  
+
       body: JSON.stringify(loginData),
     });
-  
-    const loginResponseData = await login.json();
-    console.log(loginResponseData);
-  }
 
-  useEffect(() => {
-    userAuthentification()
-  }, [])
+    const loginResponseData = await login.json();
+
+    window.localStorage.setItem('JWT', loginResponseData.jwt)
+    navigate('/')
+  };
+
+  const logout = () => {
+    window.localStorage.removeItem('JWT')
+    navigate('/')
+  }
 
   return (
     <div className="login">
       <h2>Log In</h2> <br />
-
-      <form>
-        <label> <span> Username </span>
-          <input type='text' />
+      <form className="login-form" onSubmit={handleLogin}>
+        <label>
+          {" "}
+          <span> Email </span>
+          <input
+            type="email"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
         </label>
 
-        <label> <span> Password </span>
-          <input type='text' />
+        <label>
+          {" "}
+          <span> Password </span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </label>
 
         <button>Log In</button>
       </form>
+
+      <button onClick={() => logout()}>Log Out</button>
     </div>
   );
 }

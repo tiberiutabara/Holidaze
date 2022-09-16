8b41a3822cba1724d0d3e959550e94ba2226a7c2
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-16 21:18:52 +0200

    separate pages

---
+++ src/components/Enquiries.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Enquiries() {
+  return (
+    <div>Enquiries</div>
+  )
+}
--- src/pages/Owner.jsx
+++ src/pages/Owner.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 import DeleteHotel from '../components/DeleteHotel'
+import Enquiries from '../components/Enquiries'
 import RegisterHotel from '../components/RegisterHotel'
 
 export default function Owner() {
+  const [menu, setMenu] = useState('Enquiries')
+
   return (
-    <div>
-      <RegisterHotel />
-      <DeleteHotel />
-    </div>
+    <>
+
+      <span onClick={() => setMenu('Enquiries')}>Enquiries</span>
+      <span onClick={() => setMenu('Listings')}>Listings</span>
+      
+
+      {menu === 'Enquiries' && (
+        <Enquiries />
+      )}
+    
+      {menu === 'Listings' && (
+      <div className='Listings'>
+        <RegisterHotel />
+        <DeleteHotel />
+      </div>
+      )}
+
+    </>
   )
 }

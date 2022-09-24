49357bd51b255f1d9df88ac469cda4e2d564d0a0
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-24 21:32:26 +0200

    about page finish

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,34 @@
 import "./styles/About.scss";
 import camping from '../assets/camping.svg'
+import { BiMinusCircle, BiPlusCircle} from "react-icons/bi";
+import { useState } from "react";
 
 export default function About() {
+  const [itemOne, setItemOne] = useState(false)
+  const [itemTwo, setItemTwo] = useState(false)
+  const [itemThree, setItemThree] = useState(false)
+  const [itemFour, setItemFour] = useState(false)
+  const [itemFive, setItemFive] = useState(false)
+  const [itemSix, setItemSix] = useState(false)
+
+  const toggleItem = (id) => {
+    id == 1 && setItemOne(!itemOne)
+    id == 2 && setItemTwo(!itemTwo)
+    id == 3 && setItemThree(!itemThree)
+    id == 4 && setItemFour(!itemFour)
+    id == 5 && setItemFive(!itemFive)
+    id == 6 && setItemSix(!itemSix)
+  }
+
   return (
     <div className="about">
 
       <div className="hero">
         <img src={camping} alt="camping illustration" />
-        <h1>Learn about Us</h1>
-        <h1>By Reading our Story</h1>
+        <div className="title">
+          <h2>Learn about Us</h2>
+          <h1>by Reading our Story</h1>
+        </div>
       </div>
 
       <div className="description">
@@ -32,51 +52,69 @@ export default function About() {
         </p>
       </div>
 
-      <h1>Frequently Asked Questions</h1>
-      <p>( FaQ )</p>
+      <h1 className="faq-title">Frequently Asked Questions</h1>
+      <p className="faq-undertext">( FaQ )</p>
 
       <div className="accordion">
 
         <div className="accordion-item">
-          <div className="label"><p>Can I cancel my booking?</p> <button>+</button></div>
+          <div className="label"><p>Can I cancel my booking?</p> <button onClick={() => toggleItem(1)}>{!itemOne ? <BiPlusCircle className="Switch" /> : <BiMinusCircle className="Switch" />}</button></div>
+
+          {itemOne ? (
           <div className="content"><p>Voluptatem quaerat non architecto ab
           laudantium modi minima sunt esse temporibus sint culpa, recusandae
           aliquam numquam totam ratione voluptas quod exercitationem fuga.</p></div>
+          ) : null}
         </div>
 
         <div className="accordion-item">
-          <div className="label"><p>On what occasions is it possible to get a refund?</p> <button>+</button></div>
+          <div className="label"><p>On what occasions is it possible to get a refund?</p> <button onClick={() => toggleItem(2)}>{!itemTwo ? <BiPlusCircle className="Switch" /> : <BiMinusCircle className="Switch" />}</button></div>
+
+          {itemTwo ? (
           <div className="content"><p>Voluptatem quaerat non architecto ab
           laudantium modi minima sunt esse temporibus sint culpa, recusandae
           aliquam numquam totam ratione voluptas quod exercitationem fuga.</p></div>
+          ) : null}
         </div>
 
         <div className="accordion-item">
-          <div className="label"><p>How does the business team offer work?</p> <button>+</button></div>
+          <div className="label"><p>How does the business team offer work?</p> <button onClick={() => toggleItem(3)}>{!itemThree ? <BiPlusCircle className="Switch" /> : <BiMinusCircle className="Switch" />}</button></div>
+
+          {itemThree ? (
           <div className="content"><p>Voluptatem quaerat non architecto ab
           laudantium modi minima sunt esse temporibus sint culpa, recusandae
           aliquam numquam totam ratione voluptas quod exercitationem fuga.</p></div>
+          ) : null}
         </div>
 
         <div className="accordion-item">
-          <div className="label"><p>Are there any open positions at Holidaze?</p> <button>+</button></div>
+          <div className="label"><p>Are there any open positions at Holidaze?</p> <button onClick={() => toggleItem(4)}>{!itemFour ? <BiPlusCircle className="Switch" /> : <BiMinusCircle className="Switch" />}</button></div>
+
+          {itemFour ? (
           <div className="content"><p>Voluptatem quaerat non architecto ab
           laudantium modi minima sunt esse temporibus sint culpa, recusandae
           aliquam numquam totam ratione voluptas quod exercitationem fuga.</p></div>
+          ) : null}
         </div>
 
         <div className="accordion-item">
-          <div className="label"><p>How do I open an account to list my own hotel?</p> <button>+</button></div>
+          <div className="label"><p>How do I open an account to list my own hotel?</p> <button onClick={() => toggleItem(5)}>{!itemFive ? <BiPlusCircle className="Switch" /> : <BiMinusCircle className="Switch" />}</button></div>
+
+          {itemFive ? (
           <div className="content"><p>Voluptatem quaerat non architecto ab
           laudantium modi minima sunt esse temporibus sint culpa, recusandae
           aliquam numquam totam ratione voluptas quod exercitationem fuga.</p></div>
+          ) : null}
         </div>
 
         <div className="accordion-item">
-          <div className="label"><p>Can I reschedule a booking?</p> <button>+</button></div>
+          <div className="label"><p>Can I reschedule a booking?</p> <button onClick={() => toggleItem(6)}>{!itemSix ? <BiPlusCircle className="Switch" /> : <BiMinusCircle className="Switch" />}</button></div>
+
+          {itemSix ? (
           <div className="content"><p>Voluptatem quaerat non architecto ab
           laudantium modi minima sunt esse temporibus sint culpa, recusandae
           aliquam numquam totam ratione voluptas quod exercitationem fuga.</p></div>
+          ) : null}
         </div>
 
       </div>  

ab1dd12552ce0115a524035e591def44e5d19ee1
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-25 14:59:53 +0200

    admin finish

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,20 +1,26 @@
+// General imports
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
-import Spinner from "../components/Spinner";
-import './styles/Admin.scss'
+import { useEffect, useState } from "react";
 
-const {REACT_APP_URL} = process.env
+// Style imports
+import Spinner from "../components/Spinner";
+import "./styles/Admin.scss";
+import { HiSearchCircle } from "react-icons/hi";
+
+// .env
+const { REACT_APP_URL } = process.env;
 
 export default function Admin() {
+  // States
   const navigate = useNavigate();
-  const [category, setCategory] = useState('All')
-  const [loading, setLoading] = useState(true)
-  const [messages, setMessages] = useState([])
-  const [filteredMessages, setFilteredMessages] = useState([])
+  const [category, setCategory] = useState("All");
+  const [loading, setLoading] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const [filteredMessages, setFilteredMessages] = useState([]);
   const token = window.localStorage.getItem("JWT");
 
+  // Message fetch
   useEffect(() => {
     const getMessages = async () => {
       try {
@@ -24,63 +30,58 @@ export default function Admin() {
           },
         });
         setMessages(data.data);
-        setLoading(false)
+        setLoading(false);
       } catch (err) {
-        console.log(err)
+        console.log(err);
         navigate("/login");
       }
     };
 
     getMessages();
-
   }, [navigate, token]);
 
+  // Category filter
   useEffect(() => {
-
-    if(category === 'All'){
-      setFilteredMessages(messages)
+    if (category === "All") {
+      setFilteredMessages(messages);
     } else {
-      const filterCategory = messages.filter(message => message.attributes.category === category)
-      setFilteredMessages(filterCategory)
+      const filterCategory = messages.filter(
+        (message) => message.attributes.category === category
+      );
+      setFilteredMessages(filterCategory);
     }
-  }, [category, messages])
+  }, [category, messages]);
 
   return (
     <div className="admin">
-      <h2>Inbox</h2>
+      <h1>Inbox</h1>
 
-      <label> <span>Category: </span>
-        <select 
-          id="area" 
+      <label>
+        <HiSearchCircle className="search-icon"/>
+        <select
+          id="area"
           required
           onChange={(e) => setCategory(e.target.value)}
         >
-            <option value="All">All</option>
-            <option value="Hotel Listings">Hotel Listings</option>
-            <option value="Technical Support">Technical Support</option>
-            <option value="Career">Career</option>
+          <option value="All">All Categories</option>
+          <option value="Hotel Listings">Hotel Listings</option>
+          <option value="Technical Support">Technical Support</option>
+          <option value="Career">Career</option>
         </select>
       </label>
 
       {loading && <Spinner />}
 
-      {filteredMessages.length > 0 ? (
-        filteredMessages &&
-        filteredMessages.map((message) => (
-
-
-          <div key={message.id} className="message-card">
-            <p>{message.attributes.category}</p>
-            <h3>{message.attributes.subject}</h3>
-            <p>by {message.attributes.name}</p>
-            <Link to={`/message/${message.id}`}>Details</Link>
-            <br /> <br />
-          </div>
-
-        ))
-      ) : ( !loading &&
-        <p>No messages</p>
-      )}
+      {filteredMessages.length > 0
+        ? filteredMessages &&
+          filteredMessages.map((message) => (
+            <Link key={message.id} className="message-card" to={`/message/${message.id}`}>
+              <p>{message.attributes.category}</p>
+              <h3>{message.attributes.subject}</h3>
+              <p><span>by</span> {message.attributes.name}</p>
+            </Link>
+          ))
+        : !loading && <p>No messages</p>}
     </div>
   );
 }

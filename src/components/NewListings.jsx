bf52e37f2e4b83ec24abe435df0d1261a380e0a1
tiberiutabara <tiberiutabara1@gmail.com>
2022-09-29 16:04:46 +0200

    new listings

--- src/components/NewListings.jsx
+++ src/components/NewListings.jsx
@@ -7,12 +7,14 @@ import axios from "axios"
 import Spinner from './Spinner'
 import './styles/HotelBox.scss'
 import './styles/NewListings.scss'
-import { AiFillRightCircle } from "react-icons/ai"
+import { AiFillRightCircle, AiFillDownCircle, AiFillUpCircle } from "react-icons/ai"
 
 // .env
 const {REACT_APP_URL} = process.env
 
 export default function NewListings() {
+
+  // fetch
   const [recent, setRecent] = useState([])
   const [loading, setLoading] = useState(true)
 
@@ -30,13 +32,37 @@ export default function NewListings() {
     getRecent()
   }, [])
 
+  // extender
+  const [open, setOpen] = useState(false)
+  const [shown, setShown] = useState({
+    from: 0,
+    to: 3
+  })
+
+  const onOpen = () => {
+    setOpen(!open)
+
+    open === true ?
+      setShown({
+        from: 0,
+        to: 3
+      }) 
+    : 
+      setShown({
+        from: 0,
+        to: 6
+      })
+  }
+
   return (
     <div className="new-hotels">
 
+    
     {loading && <Spinner />}
 
+    <div className="hotels-showcase">
     {recent && (
-      recent.slice(0,6).map(hotel => (
+      recent.slice(shown.from, shown.to).map(hotel => (
 
         <Link to={`/Hotel/${hotel.id}`} key={hotel.id} className="hotel-card">
         <img src={hotel.attributes.Thumbnail.data.attributes.url} alt={hotel.attributes.Title}/>
@@ -50,6 +76,13 @@ export default function NewListings() {
 
       ))
     )}
+    </div>
+
+    { !open ?
+      <AiFillDownCircle className="icon" onClick={() => onOpen()}/>
+      : 
+      <AiFillUpCircle className="icon" onClick={() => onOpen()} />
+    }
 
     </div>
   )
